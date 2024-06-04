f5238d6e0a5b4fc2508a02066888baadfd11bd4b
Alikadze <119809834+Alikadze@users.noreply.github.com>
2024-06-05 00:30:57 +0400

    fixed branch conflict

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,9 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet
+  ],
   template: '<router-outlet>',
 })
 export class AppComponent {
