ccf66a713b4fce6e040e5b54f2f34e02606da795
Alikadze <119809834+Alikadze@users.noreply.github.com>
2024-06-05 00:56:15 +0400

    fixed merged conflict

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,15 +10,11 @@ export const routes: Routes = [
       {
         path: '',
         component: HomeComponent
-      },
-      {
-        path: 'auth',
-        loadChildren: () => import('../app/pages/auth/auth.routes').then(m => m.authRoute)
       }
     ]
   },
   {
-    path: '**',
+    path: '',
     pathMatch: 'full',
     redirectTo: '/'
   }
